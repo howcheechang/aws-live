62b91f8e81a87c2d9649244ee2fb39d7ced4b06f
howcheechang <167175556+howcheechang@users.noreply.github.com>
2024-04-16 16:05:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-2.c72oi6wii0c3.us-east-1.rds.amazonaws.com"
 customuser = "admin"
-custompass = "Bait3273"
+custompass = "Aacs3253"
 customdb = "employee"
 custombucket = "changhowchee-employee"
 customregion = "us-east-1"
