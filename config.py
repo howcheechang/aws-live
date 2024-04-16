e1dad4636fe55cc2fe35c9d5048c4350eba8deac
howcheechang <167175556+howcheechang@users.noreply.github.com>
2024-04-16 15:47:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c72oi6wii0c3.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "changhowchee-employee"
+customregion = "us-east-1"
+
